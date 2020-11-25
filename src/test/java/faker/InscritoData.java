16673f2a75f2c85a7a013fa021d5c03efeb451b6
rchaname <rdchaname@gmail.com>
2020-11-25 00:57:26 -0500

    Faker data

---
+++ src/test/java/faker/InscritoData.java
@@ -0,0 +1,22 @@
+package faker;
+
+import com.github.javafaker.Faker;
+import java.util.Locale;
+import org.ceti.sistemas.proyectoceti.beans.Inscrito;
+import org.ceti.sistemas.proyectoceti.dao.InscritoDao;
+
+public class InscritoData {
+
+    public static void main(String[] args) {
+        InscritoDao inscritoDao = new InscritoDao();
+        Faker faker = new Faker(new Locale("es"));
+        for (int i = 0; i < 100; i++) {
+            Inscrito inscrito = new Inscrito();
+            inscrito.setApellido_paterno(faker.name().lastName());
+            inscrito.setApellido_materno(faker.name().lastName());
+            inscrito.setNombres(faker.name().firstName());
+            inscrito.setCelular(faker.phoneNumber().cellPhone());
+            inscritoDao.registrar(inscrito);
+        }
+    }
+}
